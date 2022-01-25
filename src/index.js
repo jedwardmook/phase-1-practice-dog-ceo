01e882b66db48b2201d6d1a9d4bc5075536429d5
John Mook <jedwardmook@yahoo.com>
2022-01-25 17:06:03 -0600

    commit tree 5cc9cf79159382b6797b954a427f1f82b37209be

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    let dogImageContainer = document.getElementById('dog-image-container');
+    let dogUL = document.querySelector("#dog-breeds")
+    //console.log(dogImageContainer);
+
+
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+        .then(function(response){
+        //console.log(response);
+        return response.json();
+    })
+        .then(function(data) {
+            let arrOfDogs = data.message;
+            arrOfDogs.forEach(function(url){
+            //dogImageContainer.innerHTML += makeImageTagString(url)
+                dogImageContainer.append(makeImgTagElement(url))
+        })
+    });
+    fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then(response => {
+            let dogBreeds = Object.keys(response.message)
+            dogBreeds.forEach((breed) => {
+                dogUL.innerHTML += `<li data-info="breed">${breed}</li>`
+            })
+        })
+    dogUL.addEventListener("click", function(e){
+        if (e.target.dataset.info === "breed"){
+            //console.log(e.target);
+            e.target.style.color = "orange"
+        }
+    })
+
+    let dogSelect = document.getElementById('breed-dropdown');
+    dogSelect.addEventListener('change',function(e){
+        fetch("https://dog.ceo/api/breeds/list/all")
+        .then(response => response.json())
+        .then (response => {
+            let dogBreeds = Object.keys(response.message)
+            let filteredArray = dogBreeds.filter(breed => {
+                return breed.startsWith(e.target.value)
+            })
+
+            dogUL.innerHTML = ""
+            filteredArray.forEach((breed) => {
+                dogUL.innerHTML += `<li data-info="breed">${breed}</li>`
+            })
+        })
+
+    })
+})
+
+//function makeImageTagString(url){
+    //return `<img src="${url}"/>`
+//}
+
+
+function makeImgTagElement(url){
+    let imgTag = document.createElement('img')
+    imgTag.src = url
+    return imgTag
+}
